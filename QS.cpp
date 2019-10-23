7805701c86899fd8fffcfa5ac00d324460ff74ea
Matt Spataro <matt_spataro@outlook.com>
2019-10-23 01:24:06 +0000

    initial setup

---
+++ QS.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include "QS.h"
+using namespace std;
+
+QS::QS() {}
+QS::~QS() {}
+void QS::sortAll(){
+    return;
+}
+int QS::medianOfThree(int left, int right){
+    return 0;
+}
+int QS::partition(int left, int right, int pivotIndex){
+    return 0;
+}
+string QS::getArray() const{
+    return "";
+}
+int QS::getSize() const{
+    return 0;
+}
+bool QS::addToArray(int value){
+    return false;
+}
+bool QS::createArray(int capacity){
+    return false;
+}
+void QS::clear(){
+    return;
+}
+
+	
